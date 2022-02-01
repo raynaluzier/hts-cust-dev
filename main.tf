9cefc7bb1b0263dd5a644292c9e2e9bef1e5f888
Rayna Luzier <rayna.luzier@thinkahead.com>
2022-02-01 14:31:11 -0500

    adfsdf

--- main.tf
+++ main.tf
@@ -12,20 +12,6 @@ provider "aws" {
   region = var.region
 }
 
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
-
-# Configure the AWS Provider
-provider "aws" {
-  region = var.region
-}
-
 module "ubuntu-vm" {
   source  = "app.terraform.io/raynaluzier/ec2-ubuntu/aws"
   version = "0.0.1"
